78864ed0d8c63f4b2f680bf71327e63535d4eb0f
bacek97 <bacek97@github.com>
2020-12-10 19:58:38 +0300

    vue-i18n

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- App.vue -->
+
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+   {{ $t('welcome') }}
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+<nuxt-link :to="localePath('index')">{{ $t('home') }}</nuxt-link>
+<nuxt-link :to="localePath('/')">{{ $t('home') }}</nuxt-link>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
+</template>
